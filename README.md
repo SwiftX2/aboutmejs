3e0601213b9aafc2e3c3238a913e6d04cdf55b9f
Jonathan Swift <jonathanswift686@gmail.com>
2022-07-31 15:57:34 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# aboutmejs
+**The link to the github page is**
+[Live Site](https://swiftx2.github.io/aboutmejs/)
---
+++ script.js
@@ -0,0 +1,10 @@
+
+let name="Nyoh Jonathan Smith";
+let height="5ft 6in"
+let country="Cameroon"
+
+
+document.write(`<h1>Hey there! \n</h1>`)
+document.write(`<h2> My name is ${name}</h2>`)
+document.write(`<h2> I am ${height} tall</h2>`)
+document.write(`<h2>I am from ${country}</h2>`)
